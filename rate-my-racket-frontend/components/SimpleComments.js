6313057a80fe2f451b7c0a6b70f7760d29703fc1
Ceci-Aguilera <ceci.aguilera@zohomail.com>
2022-04-17 15:54:21 -0400

    Most Rated Rackets section Added to Frontend

--- rate-my-racket-frontend/components/SimpleComments.js
+++ rate-my-racket-frontend/components/SimpleComments.js
@@ -6,63 +6,66 @@ import styles from "../styles/SimpleComments.module.css";
 
 import { useEffect, useState } from "react";
 
-import {ThumbsUpRegular, ThumbsDownRegular} from "./Icons"
+import { ThumbsUpRegular, ThumbsDownRegular } from "./Icons"
 
 
 function SimpleComments() {
 
-  return (
-    <div className={styles.simple_comments_div}>
+    return (
+        <div className={styles.simple_comments_div}>
 
-    <h2 className={styles.simple_comments_title}>Latest Comments</h2>
+            <h2 className={styles.simple_comments_title}>Latest Comments</h2>
 
-        {fake_comments.map((comment, index) => {
-            return (
-                <Card key={index} className={styles.simple_comments_card}>
-                    <Card.Header className={styles.simple_comments_card_header}>
-                    <div className={styles.simple_comments_card_header_avatar}></div>    <div className={styles.simple_comments_card_header_title}>{comment.username} - <span className={styles.simple_comments_card_header_span}>{comment.about}</span></div>
-                    </Card.Header>
-                    <Card.Body className={styles.simple_comments_card_body}>
-                        {comment.comment}
-                    </Card.Body>
-                    <Card.Footer className={styles.simple_comments_card_footer}>
-                        <div className={styles.simple_comments_up_votes}>
-                           <ThumbsUpRegular height={"25"} fill={"#38b6ff"} /> 
-                           <div className={styles.simple_comments_card_footer_votes_div}>
-                           {comment.up_votes}
-                           </div>
-                        </div>
+            {fake_comments.map((comment, index) => {
+                return (
+                    <Card key={index} className={styles.simple_comments_card}>
+                        <Card.Header className={styles.simple_comments_card_header}>
+                            <div className={styles.simple_comments_card_header_avatar}></div>
+                            <div className={styles.simple_comments_card_header_title}>{comment.username} - <span className={styles.simple_comments_card_header_span}>
+                                {comment.about}</span>
+                            </div>
+                        </Card.Header>
+                        <Card.Body className={styles.simple_comments_card_body}>
+                            {comment.comment}
+                        </Card.Body>
+                        <Card.Footer className={styles.simple_comments_card_footer}>
+                            <div className={styles.simple_comments_up_votes}>
+                                <ThumbsUpRegular height={"25"} fill={"#38b6ff"} />
+                                <div className={styles.simple_comments_card_footer_votes_div}>
+                                    {comment.up_votes}
+                                </div>
+                            </div>
 
-                        <div className={styles.simple_comments_down_votes}>
-                        <ThumbsDownRegular height={"25"} fill={"#aaaaaa"} /> 
-                        <div className={styles.simple_comments_card_footer_votes_div}>
-                           {comment.down_votes}
-                           </div>
-                        </div>
-                    </Card.Footer>
-                </Card>
-            )
-        })}
+                            <div className={styles.simple_comments_down_votes}>
+                                <ThumbsDownRegular height={"25"} fill={"#aaaaaa"} />
+                                <div className={styles.simple_comments_card_footer_votes_div}>
+                                    {comment.down_votes}
+                                </div>
+                            </div>
+                        </Card.Footer>
+                    </Card>
+                )
+            })}
 
-    </div>
-  );
+        </div>
+    );
 }
 
 
 
 
+//! FIX: Make comments do not excel an amount of characters for display (cut with "...")
 
 
 
-
-
+// TODO This most be fetched from the Backend API (just 3 at most)
 const fake_comments = [
     {
-    username: "User 1", about: "Babolat Pure Drive", comment: "One of the best beginner-intermediate rackets", up_votes: 5, down_votes: 3
+        username: "User 1", about: "Babolat Pure Drive", comment: "One of the best beginner-intermediate rackets", up_votes: 5, down_votes: 3
     },
     {
         username: "User 2", about: "Babolat Pure Drive", comment: "Best for beginners !!!", up_votes: 10, down_votes: 1
-        },
+    },
 
 ]
 
--- rate-my-racket-frontend/pages/index.js
+++ rate-my-racket-frontend/pages/index.js
@@ -1,9 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 import SimpleComments from '../components/SimpleComments';
+
+import top_racket_gold from "../public/images/top_racket_gold.png"
+import top_racket_silver from "../public/images/top_racket_silver.png"
+import top_racket_bronze from "../public/images/top_racket_bronze.png"
+
+import { StarIcon, StarHalfIcon, UserIcon } from '../components/Icons';
 
 export default function Home() {
   return (
@@ -15,18 +22,173 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-      <div className={styles.index_div_wrapper}>
-        <Row className={styles.index_row}>
-          <Col xs={12} sm={12} md={12} lg={8} className={styles.index_col}>
-            Some Rackets
-          </Col>
+        <div className={styles.index_div_wrapper}>
+          <Row className={styles.index_row}>
+            <Col xs={12} sm={12} md={12} lg={8} className={styles.index_col}>
 
-          <Col xs={12} sm={12} md={12} lg={4} className={styles.index_col}>
-            <SimpleComments />
-          </Col>
-        </Row>
-      </div>
+              <div className={styles.top_rackets_div}>
+
+                <h1 className={styles.top_rackets_section_title}>
+                  Most Rated Rackets
+                </h1>
+
+                <Row className={styles.top_rackets_row}>
+
+                  <Col xs={4} sm={4} md={4} lg={4} className={styles.top_rackets_col}>
+                    <div className={`${styles.top_rackets_element_div_wrapper} ${styles.top_rackets_element_div_wrapper_silver}`}>
+
+                      <Link href="/#">
+                        <div className={`${styles.top_rackets_element_div} ${styles.top_rackets_element_div_silver}`}>
+                          <Image src={top_racket_silver} layout="fill" className={styles.top_racket_img} />
+                          <div className={styles.top_racket_rate_link_div}>
+                            <p className={styles.top_racket_rate_p}>Rate This Racket</p>
+                          </div>
+                        </div>
+                      </Link>
+
+
+
+
+                      <div className={styles.top_stars_div}>
+
+                        {calculateRating(top_rackets.silver.rate, "silver").map((top_star, index) => {
+                          return (
+                            <div key={index} className={styles.top_star_div}>
+                              {top_star}
+                            </div>
+                          );
+                        })}
+
+                        <span className={styles.top_racket_amount_span}>( {top_rackets.silver.amount} <UserIcon height="15" width="15" fill={"#38b6ff"} /> )</span>
+
+                      </div>
+
+                      <p className={styles.top_rackets_p}>#2 {top_rackets.silver.name}</p>
+                    </div>
+
+
+
+                  </Col>
+
+                  <Col xs={4} sm={4} md={4} lg={4} className={styles.top_rackets_col}>
+                    <div className={`${styles.top_rackets_element_div_wrapper} ${styles.top_rackets_element_div_wrapper_gold}`}>
+
+                      <Link href="/#">
+                        <div className={`${styles.top_rackets_element_div} ${styles.top_rackets_element_div_gold}`}>
+                          <Image src={top_racket_gold} layout="fill" className={styles.top_racket_img} />
+                          <div className={styles.top_racket_rate_link_div}>
+                            <p className={styles.top_racket_rate_p}>Rate This Racket</p>
+                          </div>
+                        </div>
+                      </Link>
+
+
+
+                      <div className={styles.top_stars_div}>
+
+                        {calculateRating(top_rackets.gold.rate, "gold").map((top_star, index) => {
+                          return (
+                            <div key={index} className={styles.top_star_div}>
+                              {top_star}
+                            </div>
+                          );
+                        })}
+
+                        <span className={styles.top_racket_amount_span}>( {top_rackets.gold.amount} <UserIcon height="15" width="15" fill={"#38b6ff"} /> )</span>
+
+                      </div>
+
+                      <p className={styles.top_rackets_p}>#1 {top_rackets.gold.name}</p>
+                    </div>
+
+
+
+                  </Col>
+
+                  <Col xs={4} sm={4} md={4} lg={4} className={styles.top_rackets_col}>
+                    <div className={`${styles.top_rackets_element_div_wrapper} ${styles.top_rackets_element_div_wrapper_bonze}`}>
+
+                      <Link href="/#">
+                        <div className={`${styles.top_rackets_element_div} ${styles.top_rackets_element_div_bronze}`}>
+                          <Image src={top_racket_bronze} layout="fill" className={styles.top_racket_img} />
+                          <div className={styles.top_racket_rate_link_div}>
+                            <p className={styles.top_racket_rate_p}>Rate This Racket</p>
+                          </div>
+                        </div>
+                      </Link>
+
+
+                      <div className={styles.top_stars_div}>
+
+                        {calculateRating(top_rackets.bronze.rate, "#cd7f32").map((top_star, index) => {
+                          return (
+                            <div key={index} className={styles.top_star_div}>
+                              {top_star}
+                            </div>
+                          );
+                        })}
+
+                        <span className={styles.top_racket_amount_span}>( {top_rackets.bronze.amount} <UserIcon height="15" width="15" fill={"#38b6ff"} /> )</span>
+
+                      </div>
+
+                      <p className={styles.top_rackets_p}>#3 {top_rackets.bronze.name}</p>
+
+                    </div>
+
+
+
+                  </Col>
+
+                </Row>
+
+              </div>
+
+            </Col>
+
+            <Col xs={12} sm={12} md={12} lg={4} className={styles.index_col}>
+              <SimpleComments />
+            </Col>
+          </Row>
+        </div>
       </main>
     </div>
   )
+}
+
+const calculateRating = (rate, color) => {
+  var array_of_stars = []
+
+  const star_size = "20";
+
+  for (var i = 0; i < rate; i++) {
+    if (i + 1 <= rate) {
+      array_of_stars.push(<StarIcon height={star_size} width={star_size} fill={color} />)
+    }
+    else {
+      array_of_stars.push(<StarHalfIcon height={star_size} width={star_size} fill={color} />)
+    }
+  }
+
+  return array_of_stars
+
+}
+
+// TODO  This should be provided by the Backend API
+const top_rackets = {
+  gold: {
+    name: "BABOLAT Boost Aero",
+    rate: 5.0,
+    amount: 5000
+  },
+  silver: {
+    name: "HEAD Speed MP",
+    rate: 4.5,
+    amount: 4500
+  },
+  bronze: {
+    name: "WILSON Blade 104 V7",
+    rate: 4.8,
+    amount: 3000
+  }
 }
